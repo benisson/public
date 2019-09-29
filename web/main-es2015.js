74a5733605950aa4c4bf8b977969cba929ae0033
Benisson Lopes <benisson@gmail.com>
2019-09-29 09:07:26 -0300

    versionamento web

--- web/main-es2015.js
+++ web/main-es2015.js
@@ -497,7 +497,8 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    serverStatic: "http://localhost:8000"
+    serverStatic: ""
+    //serverStatic: "http://localhost:8000"
 };
 /*
  * For easier debugging in development mode, you can import the following file
--- web/main-es5.js
+++ web/main-es5.js
@@ -522,7 +522,8 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    serverStatic: "http://localhost:8000"
+    serverStatic: ""
+    //serverStatic: "http://localhost:8000"
 };
 /*
  * For easier debugging in development mode, you can import the following file
